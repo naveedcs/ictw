10adcafe429d6830b396415c86ee1dc31372a2a6
rajnirani-rev <revinfotech1@gmail.com>
2015-12-15 16:30:44 +0530

    index

--- js/custom.js
+++ js/custom.js
@@ -215,7 +215,7 @@ $(document).ready(function () {
         var postObject = {
             to: _toAddress,
             subject: "IChooseToWin",
-            text: "This is a share from IChooseToWin.com",
+            text: 'My Name is '+$('#name').val()+' and in '+ $('#skill').val() +' I CHOOSE TO WIN',
             imageData: _imageDataGlobal
         };
         postObject = JSON.stringify(postObject);
