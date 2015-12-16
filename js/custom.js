a32319881b66ec93e4be17621c9f85d031877690
rajnirani-rev <revinfotech1@gmail.com>
2015-12-16 14:29:34 +0530

    16dec

--- js/custom.js
+++ js/custom.js
@@ -123,11 +123,7 @@ $(document).ready(function () {
                         $("#loader").hide();
                         //$("#start_over").show();
                     },
-                    /*error: function (err,xhr, textStatus, responseText) {
-                        console.log(err);
-                        console.log(xhr.responseText);
-
-                    }*/
+                    
                     error: function(xhr, status, error) {
                       var err = eval("(" + xhr.responseText + ")");
                       console.log(err);
